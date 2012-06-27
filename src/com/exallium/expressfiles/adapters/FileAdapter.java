8ef06cb39eaa414332171462e1900afcc4ba7898
Alex Hart <alex.hart658@gmail.com>
2012-06-26 22:32:17 -0300

    Add Directory image.  I love CSS.

--- src/com/exallium/expressfiles/adapters/FileAdapter.java
+++ src/com/exallium/expressfiles/adapters/FileAdapter.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.util.List;
 
 import com.exallium.expressfiles.R;
-
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,7 @@ public class FileAdapter extends ArrayAdapter<File> {
 	
 	private List<File> objects;
 	private Context context;
+	private Drawable folderImage, defaultImage;
 
 	public FileAdapter(Context context, int textViewResourceId,
 			List<File> objects) {
@@ -24,6 +27,15 @@ public class FileAdapter extends ArrayAdapter<File> {
 		this.context = context;
 		this.objects = objects;
 		
+		Resources res = context.getResources();
+		float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48, res.getDisplayMetrics());
+		int pixel = (int) px;
+		
+		folderImage = res.getDrawable(R.drawable.directory);
+		folderImage.setBounds(0, 0, pixel, pixel);
+		
+		defaultImage = res.getDrawable(R.drawable.ic_launcher);
+		defaultImage.setBounds(0, 0, pixel, pixel);
 	}
 	
 	@Override
@@ -43,6 +55,13 @@ public class FileAdapter extends ArrayAdapter<File> {
 		
 		try {
 			title.setText(file.getName());
+			
+			if (file.isDirectory()) {
+				title.setCompoundDrawables(folderImage, null, null, null);
+			} else {
+				title.setCompoundDrawables(defaultImage, null, null, null);
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 			title.setText("<ERROR, PLEASE REFER TO LOGCAT>");
